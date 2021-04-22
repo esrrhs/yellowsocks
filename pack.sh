330877e4ede1958f153e12579b08847faa5e6215
esrrhs <esrrhs@163.com>
2021-04-22 14:16:41 +0800

    add

--- pack.sh
+++ pack.sh
@@ -15,7 +15,7 @@ for line in $build_list; do
   os=$(echo "$line" | awk -F"/" '{print $1}')
   arch=$(echo "$line" | awk -F"/" '{print $2}')
   echo "os="$os" arch="$arch" start build"
-  if [ $os != "linux" ] || [ $os != "windows" ]; then
+  if [ $os != "linux" ] && [ $os != "windows" ]; then
     continue
   fi
   CGO_ENABLED=0 GOOS=$os GOARCH=$arch go build -ldflags="-s -w"
